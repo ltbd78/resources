55c6afc2fe68ef172bedc90a4df2f7062026bf02
Linsu Han <han@azkaban.localdomain>
2021-07-16 12:20:14 -0400

    improved graph algo versatility

--- graph/algorithms.py
+++ graph/algorithms.py
@@ -1,15 +1,21 @@
 import numpy as np
 
-def get_TA(A, T, depth):
+def get_TA(A, T, depth, perserve_history=True):
     if depth < 0:
         raise Exception('Depth cannot be negative.')
     elif depth == 1:
         return T
     elif depth > 1:
-        T = T @ (A + np.identity(A.shape[0])) # identity preserves history
+        if perserve_history:
+            T = T @ (A + np.identity(A.shape[0]))
+        else:
+            T = T @ A
         depth -= 1
-        return get_TA(A, T, depth)
+        return get_TA(A, T, depth, perserve_history=perserve_history)
     
-def get_reachable_nodes(A, starting_node, depth):
+def get_reachable_nodes(A, starting_node, depth, include_starting_node=False):
     T = A[starting_node]
-    return np.where(get_TA(A, T, depth) > 0)[0]
+    TA = get_TA(A, T, depth, perserve_history=True)
+    if include_starting_node:
+        TA[starting_node] = 1
+    return np.where(TA > 0)[0]
