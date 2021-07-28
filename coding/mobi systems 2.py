2576a5925f2f83e809952725955962676a8c930f
ltbd78 <ltbd78@gmail.com>
2021-07-27 20:08:41 -0400

    new question

---
+++ coding/mobi systems 2.py
@@ -0,0 +1,5 @@
+"""
+There are around 20,000 commercial airports in the world. Given the list of all direct flights, implement a function that can answer the question: given two airports A and B, can one person travel from A to B, with one or multiple connecting flights? Assume that all flights are bi-directional, in that if there is a flight from A to B, there must be one from B to A.
+
+All flights are stored in a N x N boolean matrix: if there is a flight from A to B, then element [Row-A, Column-B] is true.
+"""
